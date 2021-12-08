44ad195db98c541d287c5c9951e9f08b3f63b225
gwanduke <gwanduke@gmail.com>
2021-12-08 20:46:51 +0900

    이미지 첨부 기능 추가

--- components/WriteEditor.js
+++ components/WriteEditor.js
@@ -1,8 +1,36 @@
-import React, {useRef} from 'react';
-import {View, StyleSheet, TextInput} from 'react-native';
+import React, {useRef, useState} from 'react';
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  Pressable,
+  Text,
+  Platform,
+  Image,
+} from 'react-native';
+import {launchImageLibrary} from 'react-native-image-picker';
 
 function WriteEditor({title, body, onChangeTitle, onChangeBody}) {
   const bodyRef = useRef();
+  const [imageRes, setImageRes] = useState();
+
+  const onSelectImage = () => {
+    launchImageLibrary(
+      {
+        mediaType: 'photo',
+        maxWidth: 512,
+        maxHeight: 512,
+        includeBase64: Platform.OS === 'android', // Google Photo를 사용하는 경우 권한 오류가 발생할 수 있으므로, base64 인코딩된 결과값을 사용해 업로드 진행
+      },
+      res => {
+        if (res.didCancel) {
+          return;
+        }
+
+        setImageRes(res);
+      },
+    );
+  };
 
   return (
     <View style={styles.block}>
@@ -25,6 +53,18 @@ function WriteEditor({title, body, onChangeTitle, onChangeBody}) {
         value={body}
         ref={bodyRef}
       />
+      <Image style={styles.image} source={{uri: imageRes?.assets[0]?.uri}} />
+      <Pressable
+        style={({pressed}) => [
+          styles.button,
+          Platform.OS === 'ios' &&
+            pressed && {
+              backgroundColor: '#efefef',
+            },
+        ]}
+        onPress={onSelectImage}>
+        <Text>이미지 첨부</Text>
+      </Pressable>
     </View>
   );
 }
@@ -43,6 +83,16 @@ const styles = StyleSheet.create({
     fontSize: 16,
     paddingVertical: 0,
     color: '#263238',
+  },
+  button: {
+    borderColor: '#000',
+    borderWidth: 1,
+    padding: 10,
+    alignItems: 'center',
+  },
+  image: {
+    width: 50,
+    height: 50,
   },
 });
 
