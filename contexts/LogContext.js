d209f52b23bf2e23fd020f73b7d0ebbc039c1774
gwanduke <gwanduke@gmail.com>
2021-12-08 20:27:50 +0900

    영구 저장소에 작성 로그 저장

--- contexts/LogContext.js
+++ contexts/LogContext.js
@@ -1,32 +1,31 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import {createContext, useState} from 'react';
 import {v4 as uuidV4} from 'uuid';
+import logsStorage from '../storages/logsStorage';
 
 const LogContext = createContext();
 
 export function LogContextProvider({children}) {
-  const [logs, setLogs] = useState([
-    {
-      id: uuidV4(),
-      title: '첫번째 로그',
-      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu laoreet nibh. Praesent lectus massa, finibus eu lectus non, congue sodales lectus. Mauris dignissim nibh massa, at ultricies ligula aliquam at. Suspendisse ex orci, tristique non ex eget, rhoncus egestas ex. In libero massa, facilisis a nulla aliquet, molestie vestibulum erat. Morbi a libero sollicitudin, accumsan dolor ut, pulvinar lacus. Quisque pellentesque sapien quis quam tincidunt egestas. Nullam suscipit tempus elit, a interdum lacus consectetur nec. Cras eu aliquam diam. Sed finibus malesuada ante, eu sodales mauris facilisis vel. Nam vitae condimentum quam, ac finibus libero. Maecenas hendrerit viverra nibh, sed pretium mauris ornare non. Nulla lectus ipsum, dignissim eu ipsum non, congue malesuada mauris. Nulla quis euismod dolor. Etiam risus dolor, vestibulum a ipsum et, ultrices condimentum leo.',
-      date: new Date(Date.now() - 1000 * 60 * 60 * 24 * 4).toISOString(),
-    },
-    {
-      id: uuidV4(),
-      title: '두번째 로그',
-      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu laoreet nibh. Praesent lectus massa, finibus eu lectus non, congue sodales lectus. Mauris dignissim nibh massa, at ultricies ligula aliquam at. Suspendisse ex orci, tristique non ex eget, rhoncus egestas ex. In libero massa, facilisis a nulla aliquet, molestie vestibulum erat. Morbi a libero sollicitudin, accumsan dolor ut, pulvinar lacus. Quisque pellentesque sapien quis quam tincidunt egestas. Nullam suscipit tempus elit, a interdum lacus consectetur nec. Cras eu aliquam diam. Sed finibus malesuada ante, eu sodales mauris facilisis vel. Nam vitae condimentum quam, ac finibus libero. Maecenas hendrerit viverra nibh, sed pretium mauris ornare non. Nulla lectus ipsum, dignissim eu ipsum non, congue malesuada mauris. Nulla quis euismod dolor. Etiam risus dolor, vestibulum a ipsum et, ultrices condimentum leo.',
-      date: new Date().toISOString(),
-    },
-    ...Array.from({length: 1})
-      .map((_, index) => ({
-        id: uuidV4(),
-        title: `Log ${index}`,
-        body: `Log ${index}`,
-        date: new Date().toISOString(),
-      }))
-      .reverse(),
-  ]);
+  const initialLogsRef = useRef(null);
+  const [logs, setLogs] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const savedLogs = await logsStorage.get();
+      if (savedLogs) {
+        initialLogsRef.current = savedLogs;
+        setLogs(savedLogs);
+      }
+    })();
+  }, []);
+
+  useEffect(() => {
+    if (logs === initialLogsRef.current) {
+      return;
+    }
+
+    logsStorage.set(logs);
+  }, [logs]);
 
   const onCreate = ({title, body, date}) => {
     const log = {
