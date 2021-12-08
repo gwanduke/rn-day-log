cfde019bf8a3f6dd7df2c068db77045206e3b1d5
gwanduke <gwanduke@gmail.com>
2021-12-08 12:53:02 +0900

    작성된 리스트 표시

--- contexts/LogContext.js
+++ contexts/LogContext.js
@@ -5,7 +5,20 @@ import {v4 as uuidV4} from 'uuid';
 const LogContext = createContext();
 
 export function LogContextProvider({children}) {
-  const [logs, setLogs] = useState([]);
+  const [logs, setLogs] = useState([
+    {
+      id: uuidV4(),
+      title: '첫번째 로그',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu laoreet nibh. Praesent lectus massa, finibus eu lectus non, congue sodales lectus. Mauris dignissim nibh massa, at ultricies ligula aliquam at. Suspendisse ex orci, tristique non ex eget, rhoncus egestas ex. In libero massa, facilisis a nulla aliquet, molestie vestibulum erat. Morbi a libero sollicitudin, accumsan dolor ut, pulvinar lacus. Quisque pellentesque sapien quis quam tincidunt egestas. Nullam suscipit tempus elit, a interdum lacus consectetur nec. Cras eu aliquam diam. Sed finibus malesuada ante, eu sodales mauris facilisis vel. Nam vitae condimentum quam, ac finibus libero. Maecenas hendrerit viverra nibh, sed pretium mauris ornare non. Nulla lectus ipsum, dignissim eu ipsum non, congue malesuada mauris. Nulla quis euismod dolor. Etiam risus dolor, vestibulum a ipsum et, ultrices condimentum leo.',
+      date: new Date(Date.now() - 1000 * 60 * 60 * 24 * 4).toISOString(),
+    },
+    {
+      id: uuidV4(),
+      title: '두번째 로그',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu laoreet nibh. Praesent lectus massa, finibus eu lectus non, congue sodales lectus. Mauris dignissim nibh massa, at ultricies ligula aliquam at. Suspendisse ex orci, tristique non ex eget, rhoncus egestas ex. In libero massa, facilisis a nulla aliquet, molestie vestibulum erat. Morbi a libero sollicitudin, accumsan dolor ut, pulvinar lacus. Quisque pellentesque sapien quis quam tincidunt egestas. Nullam suscipit tempus elit, a interdum lacus consectetur nec. Cras eu aliquam diam. Sed finibus malesuada ante, eu sodales mauris facilisis vel. Nam vitae condimentum quam, ac finibus libero. Maecenas hendrerit viverra nibh, sed pretium mauris ornare non. Nulla lectus ipsum, dignissim eu ipsum non, congue malesuada mauris. Nulla quis euismod dolor. Etiam risus dolor, vestibulum a ipsum et, ultrices condimentum leo.',
+      date: new Date().toISOString(),
+    },
+  ]);
 
   const onCreate = ({title, body, date}) => {
     const log = {
