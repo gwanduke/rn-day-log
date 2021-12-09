a74123f3fcdcf834e926043e78be4d45200af59d
gwanduke <gwanduke@gmail.com>
2021-12-09 21:19:50 +0900

    [iOS] Objective-C 이용해 NativeModule (Alert) 구현

--- NativeModuleWorkshop/App.js
+++ NativeModuleWorkshop/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import {Button, SafeAreaView} from 'react-native';
+import {Button, Platform, SafeAreaView} from 'react-native';
 import ToastModule from './Toast';
+import Alert from './Alert';
 
 function App() {
   const onPress = () => {
-    ToastModule.show('Hello World', ToastModule.SHORT);
+    if (Platform.OS === 'ios') {
+      Alert.alert('Hellow World');
+      console.log({
+        string: Alert.STRING_VALUE,
+        number: Alert.NUMBER_VALUE,
+      });
+    }
+
+    if (Platform.OS === 'android') {
+      ToastModule.show('Hello World', ToastModule.SHORT);
+    }
   };
 
   return (
