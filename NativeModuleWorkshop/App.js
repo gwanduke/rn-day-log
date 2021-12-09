9ed25bfeee41256360fc58431f31f5ce4ad0e294
gwanduke <gwanduke@gmail.com>
2021-12-09 21:47:01 +0900

    [Android] Kotlin을 이용해 NativeModule (밝기조절) 구현

--- NativeModuleWorkshop/App.js
+++ NativeModuleWorkshop/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
-import {Button, Platform, SafeAreaView} from 'react-native';
+import React, {useState} from 'react';
+import {Button, Platform, SafeAreaView, Text, View} from 'react-native';
 import ToastModule from './Toast';
 import Alert from './Alert';
+import {getBrightness, setBrightness} from './Brightness';
 
 function App() {
+  const [value, setValue] = useState(-1);
+  const onPressBrightness = async () => {
+    const brightness = await getBrightness();
+    setValue(brightness);
+  };
+
+  const onPressLow = () => {
+    setBrightness(0.25);
+  };
+
+  const onPressHigh = () => {
+    setBrightness(1);
+  };
+
   const onPress = () => {
     if (Platform.OS === 'ios') {
       Alert.alert('Hellow World');
@@ -21,6 +36,19 @@ function App() {
   return (
     <SafeAreaView>
       <Button title="Press me" onPress={onPress} />
+      {Platform.OS === 'android' && (
+        <>
+          <View
+            style={{
+              flex: 1,
+            }}>
+            <Text>{value}</Text>
+          </View>
+          <Button title="Update Brightness" onPress={onPressBrightness} />
+          <Button title="High Brightness" onPress={onPressHigh} />
+          <Button title="Low Brightness" onPress={onPressLow} />
+        </>
+      )}
     </SafeAreaView>
   );
 }
